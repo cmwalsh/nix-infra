b673f2949719403d688be4582cefc64f326e1969
Craig W <craigw@pm.me>
2024-12-01 16:12:58 +0000

    Adds 1password to the workstation profile

--- nixos/profiles/workstation.nix
+++ nixos/profiles/workstation.nix
@@ -1,10 +1,12 @@
 {...}: {
+
   imports = [
     ../desktops/kde.nix
     ../services/xserver.nix
     ../services/pipewire.nix
     ../services/flatpak.nix
     ../services/mullvad.nix
+    ../features/1password.nix
     ../features/fonts.nix
     ../features/bluetooth.nix
     ../features/plymouth.nix
