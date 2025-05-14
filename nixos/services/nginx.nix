2a8ebc0ab8ec056e4e7a0fb36a90e31e7b830d0f
Craig Walsh <319043+cmwalsh@users.noreply.github.com>
2025-05-14 20:25:12 +0100

    Moves the acme config into the nginx config

--- nixos/services/nginx.nix
+++ nixos/services/nginx.nix
@@ -1,5 +1,18 @@
 { config, ... }: {
 
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "admin@cwbt.uk";
+
+    certs."cwbt.uk" = {
+      domain = "cwbt.uk";
+      extraDomainName = [ "*.cwbt.uk" ];
+      dnsProvider = "route53";
+      dnsPropagationCheck = true;
+      credentialsFile = /run/secrets/route53-env;
+    };
+  };
+
   services.nginx = {
     enable = true;
 
@@ -8,6 +21,12 @@
         forceSSL = true;
         useACMEHost = "cwbt.uk";
         locations."/".proxyPass = "http://100.68.27.20:8096";
+      };
+
+      "ha.cwbt.uk" = {
+        forceSSL = true;
+        useACMEHost = "cwbt.uk";
+        locations."/".proxyPass = "http://100.68.27.20:8123";
       };
     };
   };
