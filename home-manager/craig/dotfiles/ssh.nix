95fd17de91e6a2c836f59571a5ad751509e2ea61
Craig Walsh <319043+cmwalsh@users.noreply.github.com>
2025-05-30 15:52:31 +0100

    Updates the ssh config for Rocinante to allow for Ghostty

--- home-manager/craig/dotfiles/ssh.nix
+++ home-manager/craig/dotfiles/ssh.nix
@@ -9,6 +9,10 @@
         hostname = "192.168.3.6";
         user = "sysadmin";
         identityFile = "~/.ssh/id_ed25519";
+        
+        setEnv = {
+          TERM = "xterm-256color";
+        };
       };
     };
   };
