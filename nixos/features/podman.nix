9a5bb879f45357400dfd6bfc429fb8081dc12eb9
Craig (Sysadmin) <319043+cmwalsh@users.noreply.github.com>
2025-06-05 20:43:58 +0100

    Disables DNS for oci containers

--- nixos/features/podman.nix
+++ nixos/features/podman.nix
@@ -6,11 +6,9 @@
     podman = {
       enable = true;
       dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
+      defaultNetwork.settings.dns_enabled = false;
     };
     
     oci-containers.backend = "podman";
   };
-
-  networking.firewall.interfaces."podman+".allowedUDPPorts = [53 5353];
 }
