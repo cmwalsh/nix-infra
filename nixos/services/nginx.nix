f00de6a61c7f307d918b3c2aaeefc9d58fd1e17a
Craig Walsh <319043+cmwalsh@users.noreply.github.com>
2025-09-22 20:15:20 +0100

    Updates nginx with Jellyfin proxy

--- nixos/services/nginx.nix
+++ nixos/services/nginx.nix
@@ -19,11 +19,21 @@
       "media.cwbt.uk" = {
         forceSSL = true;
         useACMEHost = "cwbt.uk";
-        locations."/".proxyPass = "http://192.168.3.5:8096";
+        locations."/" = {
+          proxyPass = "http://192.168.3.5:8096";
+          proxyWebsockets = true;
+
+          extraConfig = ''
+            proxy_buffering off;
+          '';
+        };
       };
     };
   };
 
   # Allow 80 and 443
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
 }
